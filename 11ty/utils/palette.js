19b6207b6ab038d8110034c7a7ec636082572f41
Trevor Morris <code@trovster.com>
2026-08-11 20:07:49 +0100

    fix: colour palette uses proportions

--- 11ty/utils/palette.js
+++ 11ty/utils/palette.js
@@ -3,9 +3,12 @@ import { getPalette } from "colorthief"
 export default async (file) => {
     const palette = await getPalette(file, {
         colorCount: 6,
-        quality: 1,
+        quality: 5,
         colorSpace: "oklch",
     })
 
-    return palette.map((color) => color.hex())
+    return palette.map((color) => ({
+        hex: color.hex(),
+        proportion: color.proportion,
+    }))
 }
