ccfa97182e3157338ccf80e825fbeef69dd34290
Trevor Morris <code@trovster.com>
2026-08-11 19:34:32 +0100

    fix: update colorthief link and quality

--- 11ty/utils/palette.js
+++ 11ty/utils/palette.js
@@ -3,6 +3,7 @@ import { getPalette } from "colorthief"
 export default async (file) => {
     const palette = await getPalette(file, {
         colorCount: 6,
+        quality: 1,
         colorSpace: "oklch",
     })
 
