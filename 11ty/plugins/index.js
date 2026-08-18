0d814d0065e8b81bb91d42d2e9a135d45008d703
Trevor Morris <code@trovster.com>
2026-08-18 08:39:20 +0100

    chore: only convert to wepb, avif removed

--- 11ty/plugins/index.js
+++ 11ty/plugins/index.js
@@ -73,7 +73,7 @@ export default (config) => {
         {
             plugin: eleventyImageTransformPlugin,
             options: {
-                formats: ["avif", "webp"],
+                formats: ["webp"],
                 htmlOptions: {
                     imgAttributes: {
                         loading: "lazy",
